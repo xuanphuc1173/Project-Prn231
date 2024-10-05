104a0d56419403beb04f59c74d88180c5890cf39
xuanphuc1173 <your_email@example.com>
2024-10-05 09:06:01 +0700

    update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12933fe1549fc9613e48b6416d9a76eacb3fbbe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955619279d087fbd47b99657aa0b232002192bbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
