24615c247b9afe8ace3fdd5281a4208516e35a1c
xuanphuc1173 <your_email@example.com>
2024-11-12 20:02:38 +0700

    update

--- MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da4a1faa687f99f3f6a345ea2474ccfc01103c85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6adeec356681c2995cf7d052111624d299d38e61")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
